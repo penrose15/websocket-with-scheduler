147f3b1efda07f186ef28f201595439e866b9ed3
penrose15 <seongju99@naver.com>
2025-12-07 20:46:38 +0900

    Docs: 주석 추가

--- src/main/java/com/websocket/api/ApiController.java
+++ src/main/java/com/websocket/api/ApiController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class ApiController {
 
     // 테스트용 api, 없어도 무방
-    @MessageMapping("/hello")
+    @MessageMapping("/hello") // /app/hello 로 들어옴 (prefix가 /app)
     @SendTo("/topic/hi")
     public Map<String, Object> websocketApi() {
         return Map.of("hello", "world");
